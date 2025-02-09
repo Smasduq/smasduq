101301ecead8c78d0504233542d5eceec7c4f8fe
Smasduq <167141074+Smasduq@users.noreply.github.com>
2025-02-09 18:22:35 +0100

    Add files via upload

--- script/home.js
+++
@@ -1,6 +0,0 @@
-let shownav = document.querySelector('.nav')
-let nav = document.querySelector('.navbtn')
-shownav.addEventListener('click', () =>  {
-    shownav.classList.toggle('.active');
-    nav.classList.toggle('.active')
-})
