ddb094970dd0b46bb27e506405a6f65f891f649d
Smasduq <167141074+Smasduq@users.noreply.github.com>
2025-02-09 10:06:19 +0100

    Update home.js

--- script/home.js
+++ script/home.js
@@ -1,6 +1,6 @@
 let shownav = document.querySelector('#check')
 let nav = document.querySelector('.navbtn')
 shownav.addEventListener('click', () =>  {
-    shownav.classList.toggle('');
+    shownav.classList.toggle('.menu');
     nav.classList.toggle('.navbtn')
 })
